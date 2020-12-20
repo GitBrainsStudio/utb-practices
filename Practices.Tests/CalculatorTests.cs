52f97d9c0f9303c07ad992a732042cdac81bd3fe
GitBrainsStudio <gitbrains@gmail.com>
2020-12-20 17:14:34 +0500

    publish commit

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(19, calculator.Additional(1, 18));
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(1, calculator.Subtraction(10, 9));
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(108, calculator.Miltiplication(12, 9));
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(5, calculator.Division(30, 6));
+        }
+
+        [Test]
+        public void DivisionMustReturnException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(100, 0));
+        }
+    }
+
+
+}
